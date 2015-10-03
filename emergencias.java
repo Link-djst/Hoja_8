ec1c29694ced1af30cd87f854f44606299628eaa
Link-djst <Link-djst@users.noreply.github.com>
2015-10-03 17:55:52 -0600

    Update emergencias.java

--- emergencias.java
+++ emergencias.java
@@ -1,22 +1,6 @@
 import java.util.*;
 public class Emergencias {
 	public static void main(String[] args){
-		Comparator<paciente> comparador = new paciente();
-		PriorityQueue<paciente> listaPaciente = new PriorityQueue<paciente>(10, comparador);
-		
-		listaPaciente.add(new paciente("Carlos", "Dolor", "D"));
-		listaPaciente.add(new paciente("Juan", "Gripe", "C"));
-		listaPaciente.add(new paciente("Chris", "Herida", "B"));
-		listaPaciente.add(new paciente("Pedro", "Hambre", "A"));
-		listaPaciente.add(new paciente("Diego", "Nada", "E"));
-		listaPaciente.add(new paciente("Mario", "Sangrado", "B"));
-		listaPaciente.add(new paciente("Luis", "Mareo", "A"));
-		
-		System.out.println("Orden de emergencia de pacientes: ");
-		while(true){
-			paciente actualPaciente = listaPaciente.poll();
-			if(actualPaciente == null) break;
-			System.out.println(actualPaciente.toString());
-		}
+		DB baseDatos = new DB("pacientes.txt");
 	}
 }
